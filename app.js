9ad2f79f86d934c6978079458c4f275b85f8fdb3
alex.mabry <alex.mabry@synapse-wireless.com>
2016-09-07 23:20:05 -0500

    update with EB details

--- app.js
+++ app.js
@@ -70,8 +70,6 @@ if (devEnv) app.use(errorHandler());
 // Wire up your routes using the express and `atlassian-connect-express` objects
 routes(app, addon);
 
-console.log(addon.config.store());
-
 // Boot the damn thing
 http.createServer(app).listen(port, function(){
   console.log('Add-on server running at http://' + os.hostname() + ':' + port);
